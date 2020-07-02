3f700d10fecac4978f416990366d9970671fd6b2
Isabel Flohr <45717230+isabelflohr@users.noreply.github.com>
2020-07-01 17:03:35 -0700

    edits

--- Db2.py
+++ Db2.py
@@ -11,10 +11,38 @@ with conn:
     conn.commit()
 conn.close()
     
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('new.db')
 with conn:
     cur = conn.cursor()
     cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
-                ('information.docx'))
+                ('information.docx',))
     conn.commit()
 conn.close()
+
+conn = sqlite3.connect('new.db')
+with conn:
+    cur = conn.cursor()
+    cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
+                ('Hello.txt',))
+    cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
+                ('myImage.png',))
+    cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
+                ('myMovie.mpg',))
+    cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
+                ('World.txt',))
+    cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
+                ('data.pdf',))
+    cur.execute('INSERT INTO tbl_fileTypes(col_fileType) VALUES (?)', \
+                ('myPhoto.jpg',))
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('new.db')
+with conn:
+    cur = conn.cursor()
+    cur.execute('SELECT col_fileType FROM tbl_fileTypes WHERE col_fileType = \
+                '?txt'')
+    varData = cur.fetchall()
+    for item in varData:
+        msg = 'Text Documents: ()'.format(item[0])
+    print(msg)
